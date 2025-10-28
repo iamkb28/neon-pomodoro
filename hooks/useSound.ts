c11adeae949f0afba8e0bdde97ca86b34c7d4ecc
iamkb28 <iamkb2808@gmail.com>
2025-10-29 01:08:00 +0530

    Add files via upload

---
+++ hooks/useSound.ts
@@ -0,0 +1,13 @@
+
+import { useState, useCallback } from 'react';
+
+export const useSound = (soundUrl: string) => {
+  const [audio] = useState(new Audio(soundUrl));
+
+  const playSound = useCallback(() => {
+    audio.currentTime = 0;
+    audio.play().catch(e => console.error("Error playing sound:", e));
+  }, [audio]);
+
+  return playSound;
+};
